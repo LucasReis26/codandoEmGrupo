7c1483306131ff7c5dee33da032f9b31da334aec
Matheusfr34 <164033400+Matheusfr34@users.noreply.github.com>
2025-04-09 11:40:14 -0300

    Update Ex2482.java

--- exercicios/ex2482/java/Ex2482.java
+++ exercicios/ex2482/java/Ex2482.java
@@ -1,1 +1,51 @@
-package ex2482.java;
+//package ex2482.java;
+
+import java.util.Scanner;
+
+class Pais{
+    String idioma;
+    String traducao;
+    public Pais(String idioma, String traducao){
+        this.idioma = idioma;
+        this.traducao = traducao;
+    }
+}
+public class Ex2482{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int quantTraducoes = sc.nextInt();
+        sc.nextLine();
+
+        Pais pais[] = new Pais[quantTraducoes];
+
+        for(int i = 0; i < quantTraducoes; i++){
+            String lingua = sc.nextLine();
+            String traducao = sc.nextLine();
+
+            pais[i] = new Pais(lingua, traducao);
+        }
+
+        int quantCriancas = sc.nextInt();
+        sc.nextLine();
+
+        String nome[] = new String[quantCriancas];
+        String idioma[] = new String[quantCriancas];
+
+        for(int i = 0; i < quantCriancas; i++){
+            nome[i] = sc.nextLine();
+            idioma[i] = sc.nextLine();
+        }
+
+        for(Pais valores : pais){
+            for(int j = 0; j < quantCriancas; j++){
+                if(idioma[j].equals(valores.idioma)){
+                    System.out.println(nome[j]);
+                    System.out.println(valores.traducao);
+                    System.out.println();
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
