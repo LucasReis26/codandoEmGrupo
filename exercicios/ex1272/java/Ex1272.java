7d599ff4db03de25d89467be7c7a0ddf629aa3ff
Lucas Reis <luctreis@gmail.com>
2025-04-24 13:31:45 -0300

    Exercicio 1272 concluido

--- exercicios/ex1272/java/Ex1272.java
+++ exercicios/ex1272/java/Ex1272.java
@@ -1,1 +1,37 @@
 package ex1272.java;
+
+import java.util.Scanner;
+
+public class Ex1272{
+	public static String mensagemOculta(String mensagem){
+		StringBuilder sb = new StringBuilder();
+		int len = mensagem.length();
+		boolean firstFound = false;
+		for(int i = 0; i < len; i++){
+			if(!(len == 1 && mensagem.charAt(0) == ' ')){
+				if(mensagem.charAt(i) != ' ' && firstFound == false){
+					sb.append(mensagem.charAt(i));
+					firstFound = true;
+				}else if(mensagem.charAt(i) == ' '){
+					firstFound = false;
+				}
+
+			}
+		}
+
+		return new String(sb);
+	}
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		Integer testes = sc.nextInt();
+		sc.nextLine();
+		for(int i = 0; i < testes; i++){
+			String mensagem = sc.nextLine();
+			System.out.println(mensagemOculta(mensagem));
+		}
+
+		sc.close();
+	}
+}
