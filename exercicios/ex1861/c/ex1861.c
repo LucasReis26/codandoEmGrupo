29b28fb02fc20cbade34b23a04d53b2e18cd17c7
Lucas Reis <luctreis@gmail.com>
2025-04-29 09:15:42 -0300

    Alteracao incompleta da logica ex 1861

--- exercicios/ex1861/c/ex1861.c
+++ exercicios/ex1861/c/ex1861.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct{
@@ -9,7 +10,7 @@ typedef struct{
 }ASSASSINO;
 
 typedef struct{
-	ASSASSINO assassinos[200000];
+	ASSASSINO *assassinos;
 	int tam;
 }HALL;
 
@@ -44,48 +45,87 @@ void quicksort(ASSASSINO *array, int esq, int dir){
 	}
 }
 
+int procuraAssassino(ASSASSINO *array, int len, char *nome){
+	int resp = -1;
+
+	for(int i = 0; i < len; i++){
+		if(strcmp(array[i].nome,nome) == 0){
+			resp = i;
+		}
+	}
+
+	return resp;
+}
+
 int main(){
 	HALL hall; 
 	hall.tam = 0;
 	
-	for(int i = 0; i < 255; i++){
-		strcpy(hall.assassinos[i].nome,"Sem nome");
-		hall.assassinos[i].assassinatos = 0;
-		hall.assassinos[i].isAlive = true;
-	}
 
 	int i = 0;
 	char nome1[11];
 	char nome2[11];
 
-	while(scanf("%s %s",nome1,nome2) == 2){
-		for(int j = 1; j <= 2; j++){
-			if(j == 1){
-				for(int i = 0; i < hall.tam + 1; i++){
-					if(strcmp(hall.assassinos[i].nome,nome1) == 0){
-						hall.assassinos[i].assassinatos++;
-						i = hall.tam + 1;
-					}else if(strcmp(hall.assassinos[i].nome,"Sem nome") == 0){
-						hall.tam++;
-						strcpy(hall.assassinos[i].nome,nome1);
-						hall.assassinos[i].assassinatos++;
-						i = hall.tam + 1;
-					}
-				}
+	while(scanf("%s",nome1) == 1){
+
+		if(hall.tam == 0){
+			hall.assassinos = (ASSASSINO *)malloc(2 * sizeof(ASSASSINO));
+
+			strcpy(hall.assassinos[hall.tam].nome,nome1);
+			hall.assassinos[hall.tam].assassinatos = 1;
+			hall.assassinos[hall.tam].isAlive = true;
+			hall.tam++;
+
+			scanf("%s",nome2);
+			strcpy(hall.assassinos[hall.tam].nome,nome2);
+			hall.assassinos[hall.tam].assassinatos = 0;
+			hall.assassinos[hall.tam].isAlive = false;
+			hall.tam++;
+		}else{
+
+			int found = procuraAssassino(hall.assassinos,hall.tam,nome1);
+
+			if(found != -1){
+				hall.assassinos[found].assassinatos++;
 			}else{
-				for(int i = 0; i < hall.tam + 1; i++){
-					if(strcmp(hall.assassinos[i].nome,nome2) == 0){
-						hall.assassinos[i].isAlive = false;
-						i = hall.tam + 1;
-					}else if(strcmp(hall.assassinos[i].nome,"Sem nome") == 0){
-						hall.tam++;
-						strcpy(hall.assassinos[i].nome,nome2);
-						hall.assassinos[i].isAlive = false;
-						i = hall.tam + 1;
-					}
-				}
+				hall.assassinos = (ASSASSINO *)realloc(hall.assassinos, (hall.tam + 1) * sizeof(ASSASSINO));
+				strcpy(hall.assassinos[hall.tam].nome,nome1);
+				hall.assassinos[hall.tam].assassinatos = 1;
+				hall.assassinos[hall.tam].isAlive = true;
+				hall.tam++;
 			}
+
 		}
+
+
+
+		// for(int j = 1; j <= 2; j++){
+		// 	if(j == 1){
+		// 		for(int i = 0; i < hall.tam + 1; i++){
+		// 			if(strcmp(hall.assassinos[i].nome,nome1) == 0){
+		// 				hall.assassinos[i].assassinatos++;
+		// 				i = hall.tam + 1;
+		// 			}else if(strcmp(hall.assassinos[i].nome,"Sem nome") == 0){
+		// 				hall.tam++;
+		// 				strcpy(hall.assassinos[i].nome,nome1);
+		// 				hall.assassinos[i].assassinatos++;
+		// 				i = hall.tam + 1;
+		// 			}
+		// 		}
+		// 	}else{
+		// 		for(int i = 0; i < hall.tam + 1; i++){
+		// 			if(strcmp(hall.assassinos[i].nome,nome2) == 0){
+		// 				hall.assassinos[i].isAlive = false;
+		// 				i = hall.tam + 1;
+		// 			}else if(strcmp(hall.assassinos[i].nome,"Sem nome") == 0){
+		// 				hall.tam++;
+		// 				strcpy(hall.assassinos[i].nome,nome2);
+		// 				hall.assassinos[i].isAlive = false;
+		// 				i = hall.tam + 1;
+		// 			}
+		// 		}
+		// 	}
+		// }
 	}
 
 	quicksort(hall.assassinos,0,hall.tam - 1);
